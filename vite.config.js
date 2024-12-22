460bdf0e900ac718e45c140f466c211e22aa24dc
ajayadav09 <ajayadav09@gmail.com>
2024-12-22 23:43:23 +0530

    update

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite';
 
 export default defineConfig({
+  base: '/sentimenter/', // Update with your repo name
   build: {
     target: 'esnext', // Ensure modern syntax is supported
   },
